07b5dbd78c7b1957c415d1a798b7be1b6ec06b45
gihun3645 <gihun3645@naver.com>
2023-01-11 19:32:04 +0900

    필드, 메소드의 오버라이딩 할 때 주의점

---
+++ src/상속/Child.java
@@ -0,0 +1,8 @@
+package 상속;
+
+public class Child extends Parent{
+    int i = 15; // 필드에 대한 오버라이딩
+    public void printI() { // 메소드에 대한 오버라이딩
+        System.out.println("child - printI() : " + i);
+    }
+}
---
+++ src/상속/ParentExam01.java
@@ -0,0 +1,18 @@
+package 상속;
+
+public class ParentExam01 {
+    public static void main(String[] args) {
+        Parent p1 = new Parent();
+        System.out.println(p1.i);
+        p1.printI();
+        System.out.println("-----------------------");
+        Child c1 = new Child();
+        System.out.println(c1.i);
+        c1.printI();
+        System.out.println("-----------------------");
+        Parent p2 = new Child(); // Child는 Parent의 후손이다. 혹은 자식이다.
+        System.out.println(p2.i);
+        p2.printII(); // 필드는 부모타입을 따라감 i는 5임 근데 웬만하면 하지 말아야함
+        p2.printI();
+    }
+}
