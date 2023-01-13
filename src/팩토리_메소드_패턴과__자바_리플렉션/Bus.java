38c1f3969619f847360de287119fd24827b8fa2c
gihun3645 <gihun3645@naver.com>
2023-01-14 02:35:18 +0900

    팩토리 메소드 패턴과 자바 리플렉션

---
+++ src/팩토리_메소드_패턴과__자바_리플렉션/Bus.java
@@ -0,0 +1,13 @@
+package 팩토리_메소드_패턴과__자바_리플렉션;
+
+public class Bus extends Car{
+    public void a() {
+        System.out.println("a");
+    }
+    public void b() {
+        System.out.println("b");
+    }
+    public void c() {
+        System.out.println("c");
+    }
+}
---
+++ src/팩토리_메소드_패턴과__자바_리플렉션/SuperCar.java
@@ -0,0 +1,7 @@
+package 팩토리_메소드_패턴과__자바_리플렉션;
+
+public class SuperCar extends Car{
+    public void a() {
+        System.out.println("supercar a!!!");
+    }
+}
