cc81ac42bd8adbe0e4aa7ac42ed9ee533ccb85b2
gihun3645 <gihun3645@naver.com>
2023-02-07 08:31:51 +0900

    - 자바 쓰레드의 개념과 사용방법

---
+++ src/thread/MyRunnable.java
@@ -0,0 +1,25 @@
+package thread;
+
+// 1. Runnable 인터페이스를 구현한다.
+public class MyRunnable implements Runnable{
+    private String str;
+
+    public MyRunnable(String str) {
+        this.str = str;
+    }
+
+    // 2. run() 메소드를 오버라이딩 한다.
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        System.out.println("--- "+name+" ---");
+        for(int i=0; i<10; i++) {
+            System.out.print(str);
+            try {
+                Thread.sleep(1000); // 1초간 쉰다.
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } // for
+    }
+}
--- src/회원관리/UserUI.java
+++ src/회원관리/UserUI.java
@@ -3,7 +3,6 @@ package 회원관리;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Iterator;
-import java.util.List;
 
 public class UserUI {
     private BufferedReader br;
