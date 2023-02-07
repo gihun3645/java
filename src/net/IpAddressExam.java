16c63a2ea837b8800288457b031547260ac385c2
gihun3645 <gihun3645@naver.com>
2023-02-07 12:02:47 +0900

    - IP 주소와 PORT - 127.0.0.1 - IP주소 알아내기 - localhost - 아주 아주 간단한 웹서버 만들기

---
+++ src/net/IpAddressExam.java
@@ -0,0 +1,24 @@
+package net;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class IpAddressExam {
+    public static void main(String[] args) {
+        try {
+            InetAddress ia = InetAddress.getLocalHost(); // 내 컴퓨터의 IP 정보를 구현한다.
+            System.out.println(ia.getHostAddress());
+        } catch (UnknownHostException uhe) {
+            uhe.printStackTrace();
+        }
+
+        try {
+            InetAddress[] isArray = InetAddress.getAllByName("google.com"); // 내 컴퓨터의 IP 정보를 구현한다.
+            for(InetAddress ia: isArray) {
+                System.out.println(ia.getHostAddress());
+            }
+        } catch (UnknownHostException uhe) {
+            uhe.printStackTrace();
+        }
+    }
+}
