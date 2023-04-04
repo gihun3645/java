5dfa54033ec82b6581142e4d55b2768a4eb8c848
gihun3645 <gihun3645@naver.com>
2023-04-05 02:51:14 +0900

    학교 자바 수업 - 4주차 과제(수정)

--- src/학교_자바수업/week4/DrawCircles.java
+++ src/학교_자바수업/week4/DrawCircles.java
@@ -6,10 +6,15 @@ import java.awt.*;
 public class DrawCircles extends JFrame {
 
     // 시작 위치
-    private static final int X = 50;
-    private static final int Y = 50;
-    private static final int CIRCLE_SIZE_INCREMENT = 15;
-    private static final int TOTAL_NUM_OF_CIRCLES = 20;
+    private static final double X = 50;
+    private static final double Y = 50;
+    // 증가 크기
+    private static final double CIRCLE_SIZE_INCREMENT = 15;
+    // 원의 개수
+    private static final double TOTAL_NUM_OF_CIRCLES = 20;
+    // 원의 시작 크기
+    private static final double START_CIRCLE_SIZE = 3;
+    // 색상
     private static final Color[] COLORS = {Color.RED, Color.GREEN, Color.BLUE};
 
     private JPanel panel;
@@ -18,17 +23,20 @@ public class DrawCircles extends JFrame {
         panel = new JPanel() {
             @Override
             public void paint(Graphics g) {
-                int x = X, y = Y;
-                int colorIndex = 0;
-                for (int i = 1; i <= TOTAL_NUM_OF_CIRCLES; i++) {
-                    g.setColor(COLORS[colorIndex]);
-                    g.fillOval(x, y, i * CIRCLE_SIZE_INCREMENT, i * CIRCLE_SIZE_INCREMENT);
+                double x = X, y = Y;
+                double colorIndex = 0;
+                for (double i = START_CIRCLE_SIZE; i < (int)(TOTAL_NUM_OF_CIRCLES+START_CIRCLE_SIZE); i++) {
+                    g.setColor(COLORS[(int) colorIndex]);
+                    // 지정된 좌표(x, y)에서 JPanel에 채워진 타원을 그림
+                    g.fillOval((int) x, (int) y, (int) (i * CIRCLE_SIZE_INCREMENT), (int) (i * CIRCLE_SIZE_INCREMENT));
+                    // 루프를 반복할 때마다 고정된 양만큼 x 및 y 좌표를 증가시켜 원을 이동시킴
                     x += 5;
                     y += 5;
                     colorIndex = (colorIndex + 1) % COLORS.length;
                 }
             }
         };
+        //  DrawCircles 클래스의 JFrame 창에 panel 개체를 추가하는 메서드 호출
         add(panel);
     }
 }
