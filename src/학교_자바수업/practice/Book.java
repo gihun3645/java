d6dd01ed1d0325ed25a1167644eb3edb7860911b
gihun3645 <gihun3645@naver.com>
2023-05-23 14:01:50 +0900

    5/23 학교 수업 복습 - 오버로딩, 오버라이딩

--- src/학교_자바수업/practice/Book.java
+++ src/학교_자바수업/practice/Book.java
@@ -1,39 +1,44 @@
 package 학교_자바수업.practice;
 
 public class Book {
-    private static int bookCount = 0;
     private String title;
     private String author;
-    private int price;
 
-    public Book() {
-        bookCount++;
-    }
-
-    public Book(String title, String author, int price) {
+    public Book(String title, String author) {
         this.title = title;
         this.author = author;
-        this.price = price;
-        bookCount++;
     }
 
-    public void setValue(String title, String author, int price) {
-        this.title = title;
-        this.author = author;
-        this.price = price;
+
+    // equals(Object oBook)상황에서는
+    // 슈퍼 클래스의 참조 변수로는 서브클래스 맴버를 접근하지 못한다.
+    // 대충 별로 안좋다는 말?
+    // instanceof 연산자를 사용해야함
+//    public boolean equals(Object oBook) {
+//        return title.equals(oBook.title)
+//                && author.equals(oBook.author);
+//    }
+    public boolean equals(Object obj) {
+        if (obj != null && obj instanceof Book) {
+            // 매개변수를 Book 타입으로 다운케스팅
+            Book oBook = (Book) obj;
+            return title.equals(oBook.title)
+                    && author.equals(oBook.author);
+        }
+        else
+            return false;
     }
 
-    public void discountBy(int percent) {
-        int discount = (int) (price * percent / 100.0);
-        price -= discount;
-    }
-
-    public static int getBookCount() {
-        return bookCount;
-    }
-
-    @Override
+    // Object의 toString 메소드는 별로라 각 클래스에 맞게 오버라이딩 하는게 나음
     public String toString() {
-        return "제목=" + title + ", 저자=" + author + ", 가격=" + price + "원";
+        return "Book [title=" + title + ", author=" + author + "]";
+    }
+
+    public static void main(String[] args) {
+        Book b1 = new Book("벌레 이야기", "이청준");
+        System.out.println("b1 = " + b1.toString());
+        Book b2 = new Book("벌레 이야기", "이청준");
+        System.out.println("b2 = " + b2.toString());
+        System.out.println("b1.eqauls(b2) : " + b1.equals(b2)); // 문자열이 같아서 true를 반환
     }
 }
